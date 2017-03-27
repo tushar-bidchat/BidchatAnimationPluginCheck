296d6efb134af305aab18d35158b2a05ffb02040
Tushar Vengurlekar <tushar.vengurlekar@bidchat.com>
2017-03-27 15:14:07 +0530

    Added Code to check new animation with location check

--- www/js/index.js
+++ www/js/index.js
@@ -38,7 +38,27 @@ var app = {
         });
 
         document.getElementById("buttonLikes").addEventListener("click", function () {
-            cordova.plugins.BidchatAnimations.likes(null, null)
+            var frame = elementPosition(document.getElementById('buttonLikes'))
+            var userId = "1";
+            var userName = "Tushar";
+            var userImageUrl = "https://lh3.googleusercontent.com/-Y86IN-vEObo/AAAAAAAAAAI/AAAAAAAKyAM/6bec6LqLXXA/s0-c-k-no-ns/photo.jpg";
+            cordova.plugins.BidchatAnimations.likes(null, null, frame.x, frame.y, frame.toplocation, userId, userName, userImageUrl);
+        });
+
+        document.getElementById("buttonLol").addEventListener("click", function () {
+            var frame = elementPosition(document.getElementById('buttonLol'))
+            var userId = "1";
+            var userName = "Tushar";
+            var userImageUrl = "https://lh3.googleusercontent.com/-Y86IN-vEObo/AAAAAAAAAAI/AAAAAAAKyAM/6bec6LqLXXA/s0-c-k-no-ns/photo.jpg";
+            cordova.plugins.BidchatAnimations.lol(null, null, frame.x, frame.y, frame.toplocation, userId, userName, userImageUrl);
+        });
+
+        document.getElementById("buttonMarryMe").addEventListener("click", function () {
+            var frame = elementPosition(document.getElementById('buttonMarryMe'))
+            var userId = "2";
+            var userName = "Swarupa";
+            var userImageUrl = "https://lh3.googleusercontent.com/-Y86IN-vEObo/AAAAAAAAAAI/AAAAAAAKyAM/6bec6LqLXXA/s0-c-k-no-ns/photo.jpg";
+            cordova.plugins.BidchatAnimations.marryMe(null, null, frame.x, frame.y, frame.toplocation, userId, userName, userImageUrl);
         });
     }
 };
@@ -76,3 +96,42 @@ var onCountdownComplete = function () {
     isCountdownTimerOn = false;
     document.getElementById("buttonCountdown").innerHTML = "Countdown Start";
 }
+
+/*
+ ================================
+ Get Element position in the Dom
+ ================================
+ */
+elementPosition = function (el) {
+    var xPos = 0;
+    var yPos = 0;
+    var viewPortHeight = $(window).height();
+    var oneFourthHeight = viewPortHeight / 3;
+    var toplocation = 0;
+
+    while (el) {
+        if (el.tagName == "BODY") {
+            // deal with browser quirks with body/window/document and page scroll
+            var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+            var yScroll = el.scrollTop || document.documentElement.scrollTop;
+
+            xPos += (el.offsetLeft - xScroll + el.clientLeft);
+            yPos += (el.offsetTop - yScroll + el.clientTop);
+        } else {
+            // for all other non-BODY elements
+            xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+            yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+        }
+
+        el = el.offsetParent;
+    }
+
+    if (yPos > oneFourthHeight)
+        toplocation = yPos - oneFourthHeight;
+
+    return {
+        x: ""+xPos+"",
+        y: ""+yPos+"",
+        toplocation: ""+toplocation+""
+    };
+};
